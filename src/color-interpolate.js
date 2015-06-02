83f9f2a9cdbac479d2d522c7db462304eac79b00
leozdgao <gzdleo0816@gmail.com>
2015-06-02 22:48:14 +0800

    test interpolate

---
+++ src/color-interpolate.js
@@ -0,0 +1,26 @@
+var d3 = require('d3');
+
+exports.draw = function(count, range) {
+  var colorScale = d3.scale.linear()
+    .domain([0, count])
+    .range(range);
+  var data = [];
+  for(var i = 0; i < count; i++) data.push(i);
+  
+  getSelection().data(data)
+    .enter()
+    .append('div')
+    .classed('color-cell', true)
+    .append('span');
+  
+  getSelection().data(data)
+    .style('background-color', function(d) {
+      return colorScale(d);
+    })
+    .select('span')
+    .text(function(d, i) { return i; });
+    
+  function getSelection() {
+    return d3.select('#color-gallery').selectAll('.color-cell');
+  }
+};
--- src/main.js
+++ src/main.js
@@ -8,15 +8,16 @@ var btnAdd = document.getElementById('btn-add');
 btnAdd.addEventListener('click', function() {
   
 });
-//setInterval(function () {
-//  barChart.draw(function() {
-//    data.shift();
-//    data.push(rand.next());
-//    return data;
-//  });
-//}, 1000);
+setInterval(function () {
+  barChart.draw(function() {
+    data.shift();
+    data.push(rand.next());
+    return data;
+  });
+}, 1000);
 
 require('./three-circle').draw(service.randomArray(100, 300, 5));
 require('./line-chart').draw(service.randomArray(1, 20, 12));
 
 console.dir(require('./working_with_array'));
+require('./color-interpolate').draw(20, ['steelblue', 'white']);
